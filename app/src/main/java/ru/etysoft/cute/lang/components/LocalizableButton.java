7537007fd2b2e6019a95f1f7a27b51129475817e
karlov <51130679+karlovm@users.noreply.github.com>
2021-07-22 00:53:24 +0300

    CustomLanguage implementation - CustomLanguage.java - LocalizableButton.java - LocalizableEditText.java

--- app/src/main/java/ru/etysoft/cute/lang/components/LocalizableButton.java
+++ app/src/main/java/ru/etysoft/cute/lang/components/LocalizableButton.java
@@ -1,39 +1,42 @@
 package ru.etysoft.cute.lang.components;
 
+
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.StyleableRes;
-import androidx.appcompat.widget.AppCompatTextView;
+import androidx.appcompat.widget.AppCompatButton;
+
+import org.jetbrains.annotations.NotNull;
 
 import ru.etysoft.cute.R;
 import ru.etysoft.cute.exceptions.NoSuchValueException;
 import ru.etysoft.cute.lang.StringsRepository;
 
-
-public class LocalizableTextView extends AppCompatTextView {
-
+public class LocalizableButton extends AppCompatButton {
     @StyleableRes
     int localizableKey = 0;
     private CharSequence localId;
 
-    public LocalizableTextView(Context context) {
+    public LocalizableButton(Context context) {
         super(context);
     }
 
-    public LocalizableTextView(Context context, AttributeSet attrs) {
+    public LocalizableButton(@NonNull @NotNull Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs) {
         super(context, attrs);
         initialize(context, attrs);
     }
 
-    public LocalizableTextView(Context context, AttributeSet attrs, int defStyle) {
+    public LocalizableButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         initialize(context, attrs);
     }
 
     private void initialize(Context context, AttributeSet attrs) {
-        int[] sets = {R.attr.localizableKey};
+        int[] sets = {R.attr.localizableButtonKey};
         TypedArray typedArray = context.obtainStyledAttributes(attrs, sets);
         CharSequence locId = typedArray.getText(localizableKey);
         localId = locId;
@@ -43,6 +46,4 @@ public class LocalizableTextView extends AppCompatTextView {
         }
         typedArray.recycle();
     }
-
-
 }
--- app/src/main/java/ru/etysoft/cute/lang/components/LocalizableEditText.java
+++ app/src/main/java/ru/etysoft/cute/lang/components/LocalizableEditText.java
@@ -4,45 +4,42 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.StyleableRes;
-import androidx.appcompat.widget.AppCompatTextView;
+import androidx.appcompat.widget.AppCompatEditText;
+
+import org.jetbrains.annotations.NotNull;
 
 import ru.etysoft.cute.R;
 import ru.etysoft.cute.exceptions.NoSuchValueException;
 import ru.etysoft.cute.lang.StringsRepository;
 
-
-public class LocalizableTextView extends AppCompatTextView {
+public class LocalizableEditText extends AppCompatEditText {
 
     @StyleableRes
     int localizableKey = 0;
     private CharSequence localId;
 
-    public LocalizableTextView(Context context) {
-        super(context);
-    }
-
-    public LocalizableTextView(Context context, AttributeSet attrs) {
+    public LocalizableEditText(@NonNull @NotNull Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs) {
         super(context, attrs);
         initialize(context, attrs);
     }
 
-    public LocalizableTextView(Context context, AttributeSet attrs, int defStyle) {
+    public LocalizableEditText(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         initialize(context, attrs);
     }
 
     private void initialize(Context context, AttributeSet attrs) {
-        int[] sets = {R.attr.localizableKey};
+        int[] sets = {R.attr.localizableHint};
         TypedArray typedArray = context.obtainStyledAttributes(attrs, sets);
         CharSequence locId = typedArray.getText(localizableKey);
         localId = locId;
         try {
-            setText(StringsRepository.getValue(String.valueOf(locId)));
+            setHint(StringsRepository.getValue(String.valueOf(locId)));
         } catch (NoSuchValueException ignored) {
         }
         typedArray.recycle();
     }
-
-
 }
